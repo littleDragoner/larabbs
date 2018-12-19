116f03901c432f3c42befcdb4fb947a41d212159
littledragoner <1559897062@qq.com>
2018-12-19 07:13:21 +0000

    Sidebar active users

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Request $request,Topic $topic)
+    public function show(Category $category,Request $request,Topic $topic,User $user)
     {
         $topics = $topic->WithOrder($request->order)
                         ->where('category_id',$category->id)
                         ->paginate(20);
-        return view('topics.index',compact('topics','category'));
+
+        $active_users = $user->getActiveUsers();
+
+        return view('topics.index',compact('topics','category','active_users'));
     }
 }
--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,7 +9,7 @@ use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Auth;
 use App\Handlers\ImageUploadHandler;
-
+use App\Models\User;
 class TopicsController extends Controller
 {
     public function __construct()
@@ -17,10 +17,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, Topic $topic,User $user)
     {
         $topics = Topic::withOrder($request->order)->paginate(30);
-        return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics','active_users'));
     }
 
     public function show(Request $request,Topic $topic)
